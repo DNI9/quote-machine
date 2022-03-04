56e2fedda4dde9bb148b62ba19e028a2ba357ca4
DNI9 <itsr9boi@gmail.com>
2022-03-04 14:54:56 +0530

    add author page

---
+++ src/author.ts
@@ -0,0 +1,48 @@
+import {getQuoteByAuthor} from './lib/quote';
+import './styles/author.css';
+import './styles/globals.css';
+import './styles/header.css';
+import './styles/main.css';
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const author = urlParams.get('author');
+{
+  `
+	<figure>
+				<blockquote id="quote">
+					<p>Loading a great quote...</p>
+				</blockquote>
+				<figcaption id="author">
+					Author
+				</figcaption>
+			</figure>
+			`;
+}
+if (!author) window.location.replace('/');
+
+const title = document.querySelector<HTMLHeadingElement>('main .title')!;
+const quotesSection = document.getElementById('quotes')!;
+
+const fetchQuotes = async () => {
+  try {
+    const quotes = await getQuoteByAuthor(author!);
+    title.innerText = `Quotes by ${quotes[0].author}`;
+
+    quotes.forEach(q => {
+      quotesSection.innerHTML += `<figure>
+				<blockquote id="quote">
+					<p>${q.content}</p>
+				</blockquote>
+			</figure>
+			`;
+    });
+  } catch (error) {
+    console.error(error);
+    alert('Failed to get quotes');
+  } finally {
+    console.log('fetched');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', fetchQuotes);
