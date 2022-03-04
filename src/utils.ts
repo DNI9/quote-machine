681127a8fbcc6b10d63ff7b06c3d6efeb75cef10
DNI9 <itsr9boi@gmail.com>
2022-03-04 12:02:45 +0530

    use classlist toggle

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,3 @@
 export const toggleLoading = (element: HTMLElement | any) => {
-  if (!element.classList.contains('loading')) {
-    element.classList.add('loading');
-  } else {
-    element.classList.remove('loading');
-  }
+  element.classList.toggle('loading');
 };
